eb622a39cb5c796ddc90b38339b2940203b254e2
Amanda Veiga <49622800+mandaveiga@users.noreply.github.com>
2021-09-27 21:19:55 -0300

    renderiza tela

--- implementacao/src/main/java/locadora/controller/ClienteController.java
+++ implementacao/src/main/java/locadora/controller/ClienteController.java
@@ -2,12 +2,25 @@ package locadora.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ClienteController {
 
     @RequestMapping("/cliente/form")
     public String indexteste(){
+        return "cliente/form";
+    }
+
+    @RequestMapping("/cliente/cadastrar")
+    public String a(){
+        String teste = "a";
+        return "cliente/form";
+    }
+
+    @RequestMapping(value="/cliente/cadastrar", method=RequestMethod.POST)
+    public String cadastrar(){
+        String teste = "a";
         return "cliente/form";
     }
 }
